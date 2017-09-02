d7138b639ce683d433693547a5bfcaaa9c2ed62e
prsp20 <prsp20@192.168.0.3>
2017-09-02 23:43:32 +0300

    Add command pattern example

---
+++ behavioral-pattern/src/main/java/com/prakass/designpattern/command/ClientDemo.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.prakass.designpattern.command;
+
+public class ClientDemo {
+    public static void main(String[] args) {
+        CarEngine engine = new CarEngine();
+        StartCommand startCommand = new StartCommand(engine);
+        StopCommand stopCommand = new StopCommand(engine);
+
+        CarControlDashboard dashboard = new CarControlDashboard();
+
+        dashboard.executeCommand(startCommand);
+        dashboard.executeCommand(stopCommand);
+    }
+}
---
+++ behavioral-pattern/src/test/java/com/prakass/designpattern/command/StopCommandTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.prakass.designpattern.command;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+
+public class StopCommandTest {
+
+    @Test
+    public void shouldStopTheEngine() {
+        CarEngine engine = mock(CarEngine.class);
+
+        StopCommand stopCommand = new StopCommand(engine);
+        stopCommand.execute();
+
+        verify(engine).stop();;
+    }
+}
