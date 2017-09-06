0e97a2a6054b1fec1a7d02896ca9ed93f4840939
prsp20 <prsp20@gmail.com>
2017-09-06 23:00:30 +0300

    Add state pattern example

---
+++ behavioral-pattern/src/main/java/com/prakass/designpattern/state/BluetoothInputState.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2017 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.prakass.designpattern.state;
+
+/**
+ * Concrete state. It knows only about next state. It does not care about any other other states.
+ *
+ */
+public class BluetoothInputState extends InputSourceState {
+
+    public BluetoothInputState(MusicPlayerInputSourceButton button) {
+        super(button);
+    }
+
+    @Override
+    public void pressInputSourceButton() {
+        button.setInputSource(new Hdmi1InputState(button));
+    }
+
+    @Override
+    public String toString() {
+        return "Bluetooth input source";
+    }
+}
